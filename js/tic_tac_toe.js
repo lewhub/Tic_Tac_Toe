1a1bc659269687b4df4f1c89d01976eebc86349b
lewhub <bracey.lewis@gmail.com>
2016-01-25 12:34:17 -0800

    added a comment

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -3,3 +3,5 @@ var board = document.getElementById('board')
 for (var i = 0; i < 9; i += 1) {
   board.innerHTML += "<div></div>"
 }
+// this is a test comment
+// to demonstrate what happens when you switch branches
